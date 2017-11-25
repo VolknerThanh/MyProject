d28b882b626ddcac460505f78d9d75cf3efa6619
Silver Soul <nhankimthanh1997@gmail.com>
2017-11-26 01:09:35 +0700

    Add files via upload

---
+++ database/allshoesDB.php
@@ -0,0 +1,486 @@
+<?php
+
+function getAllShoes(){
+	return array(
+		array(
+			"name" => "Nike 1",
+			"cost" =>"2 500 000",
+			"price"=>"2500000",
+			"img" =>"img/Nike/Nike-1.png",
+			"tag"=>"nike",
+			"status"=>"new"
+		),
+		array(
+			"name" =>"Nike 2",
+			"cost" =>"3 100 000",
+			"price"=>"3100000",
+			"img" =>"img/Nike/Nike-2.jpg",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 3",
+			"cost" =>"2 950 000",
+			"price"=>"2950000",
+			"img" =>"img/Nike/Nike-3.png",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 4",
+			"cost" =>"3 250 000",
+			"price"=>"3250000",
+			"img" =>"img/Nike/Nike-4.png",
+			"tag"=>"nike",
+			"status"=>"new"
+		),
+		array(
+			"name" =>"Nike 5",
+			"cost" =>"3 500 000",
+			"price"=>"3500000",
+			"img" =>"img/Nike/Nike-5.png",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 6",
+			"cost" =>"3 200 000",
+			"price"=>"3200000",
+			"img" =>"img/Nike/Nike-6.jpg",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 7",
+			"cost" =>"3 000 000",
+			"price"=>"3000000",
+			"img" =>"img/Nike/Nike-7.jpg",
+			"tag"=>"nike",
+			"status"=>"new"
+		),
+		array(
+			"name" =>"Nike 8",
+			"cost" =>"2 800 000",
+			"price"=>"2800000",
+			"img" =>"img/Nike/Nike-8.png",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 9",
+			"cost" =>"3 800 000",
+			"price"=>"3800000",
+			"img" =>"img/Nike/Nike-9.png",
+			"tag"=>"nike",
+			"status"=>"new"
+		),
+		array(
+			"name" =>"Nike 10",
+			"cost" =>"3 700 000",
+			"price"=>"2700000",
+			"img" =>"img/Nike/Nike-10.jpg",
+			"tag"=>"nike",
+			"status"=>""
+		),
+		array(
+			"name" =>"Nike 11",
+			"cost" =>"3 550 000",
+			"price"=>"3550000",
+			"img" =>"img/Nike/Nike-11.png",
+			"tag"=>"nike",
+			"status"=>""
+		),array(
+			"name"=>"Adidas 1",
+			"cost"=>"2 000 000",
+			"price"=>"2000000",
+			"img"=>"img/Adidas/Adidas-1.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 2",
+			"cost"=>"2 500 000",
+			"price"=>"2500000",
+			"img"=>"img/Adidas/Adidas-2.jpg",
+			"tag"=>"adidas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Adidas 3",
+			"cost"=>"2 500 000",
+			"price"=>"2500000",
+			"img"=>"img/Adidas/Adidas-3.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 4",
+			"cost"=>"3 000 000",
+			"price"=>"3000000",
+			"img"=>"img/Adidas/Adidas-4.jpg",
+			"tag"=>"adidas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Adidas 5",
+			"cost"=>"2 800 000",
+			"price"=>"2800000",
+			"img"=>"img/Adidas/Adidas-5.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 6",
+			"cost"=>"3 200 000",
+			"price"=>"3200000",
+			"img"=>"img/Adidas/Adidas-6.jpg",
+			"tag"=>"adidas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Adidas 7",
+			"cost"=>"2 200 000",
+			"price"=>"2200000",
+			"img"=>"img/Adidas/Adidas-7.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 8",
+			"cost"=>"2 000 000",
+			"price"=>"2000000",
+			"img"=>"img/Adidas/Adidas-8.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 9",
+			"cost"=>"2 500 000",
+			"price"=>"2500000",
+			"img"=>"img/Adidas/Adidas-9.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Adidas 10",
+			"cost"=>"3 000 000",
+			"price"=>"3000000",
+			"img"=>"img/Adidas/Adidas-10.jpg",
+			"tag"=>"adidas",
+			"status"=>""
+		),array(
+			"name"=>"Converse 1",
+			"cost"=>"3 000 000",
+			"price"=>"3000000",
+			"img"=>"img/Converse/Converse-1.1.jpg",
+			"tag"=>"converse",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Converse 2",
+			"cost"=>"1 800 000",
+			"price"=>"1800000",
+			"img"=>"img/Converse/Converse-2.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 3",
+			"cost"=>"2 500 000",
+			"price"=>"2500000",
+			"img"=>"img/Converse/Converse-3.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 4",
+			"cost"=>"2 990 000",
+			"price"=>"2990000",
+			"img"=>"img/Converse/Converse-4.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 5",
+			"cost"=>"2 300 000",
+			"price"=>"2300000",
+			"img"=>"img/Converse/Converse-5.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 6",
+			"cost"=>"2 200 000",
+			"price"=>"2200000",
+			"img"=>"img/Converse/Converse-6.1.jpg",
+			"tag"=>"converse",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Converse 7",
+			"cost"=>"2 800 000",
+			"price"=>"2800000",
+			"img"=>"img/Converse/Converse-7.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 8",
+			"cost"=>"3 000 000",
+			"price"=>"3000000",
+			"img"=>"img/Converse/Converse-8.1.jpg",
+			"tag"=>"converse",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Converse 9",
+			"cost"=>"2 990 000",
+			"price"=>"2990000",
+			"img"=>"img/Converse/Converse-9.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Converse 10",
+			"cost"=>"1 800 000",
+			"price"=>"1800000",
+			"img"=>"img/Converse/Converse-10.1.jpg",
+			"tag"=>"converse",
+			"status"=>""
+		),
+		array(
+			"name"=>"Luoi 1",
+			"cost"=>"800 000",
+			"price"=>"800000",
+			"img"=>"img/Ananas/Luoi-1.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 1",
+			"cost"=>"700 000",
+			"price"=>"700000",
+			"img"=>"img/Ananas/Neaker-1.jpg",
+			"tag"=>"ananas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Neaker 2",
+			"cost"=>"550 000",
+			"price"=>"550000",
+			"img"=>"img/Ananas/Neaker-2.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 3",
+			"cost"=>"400 000",
+			"price"=>"400000",
+			"img"=>"img/Ananas/Neaker-3.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 4",
+			"cost"=>"550 000",
+			"price"=>"550000",
+			"img"=>"img/Ananas/Neaker-4.jpg",
+			"tag"=>"ananas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Neaker 5",
+			"cost"=>"600 000",
+			"price"=>"600000",
+			"img"=>"img/Ananas/Neaker-5.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 6",
+			"cost"=>"500 000",
+			"price"=>"500000",
+			"img"=>"img/Ananas/Neaker-6.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 7",
+			"cost"=>"800 000",
+			"price"=>"800000",
+			"img"=>"img/Ananas/Neaker-7.jpg",
+			"tag"=>"ananas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Neaker 8",
+			"cost"=>"520 000",
+			"price"=>"520000",
+			"img"=>"img/Ananas/Neaker-8.png",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 9",
+			"cost"=>"490 000",
+			"price"=>"490000",
+			"img"=>"img/Ananas/Neaker-9.jpg",
+			"tag"=>"ananas",
+			"status"=>""
+		),
+		array(
+			"name"=>"Neaker 10",
+			"cost"=>"790 000",
+			"price"=>"790000",
+			"img"=>"img/Ananas/Neaker-10.jpg",
+			"tag"=>"ananas",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"BST 1",
+			"cost"=>"500 000",
+			"price"=>"500000",
+			"img"=>"img/Hunter/BST-M-1.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""
+		),
+		array(
+			"name"=>"Feast 1",
+			"cost"=>"500 000",
+			"price"=>"500000",
+			"img"=>"img/Hunter/Feast-M-1.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""	
+		),
+		array(
+			"name"=>"Feast 2",
+			"cost"=>"550 000",
+			"price"=>"550000",
+			"img"=>"img/Hunter/Feast-M-2.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""
+		),
+		array(
+			"name"=>"Feast 3",
+			"cost"=>"520 000",
+			"price"=>"520000",
+			"img"=>"img/Hunter/Feast-M-3.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""
+		),
+		array(
+			"name"=>"Liteknit 1",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Liteknit-M-1.jpg",
+			"tag"=>"hunter-male",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Liteknit 2",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Liteknit-M-2.jpg",
+			"tag"=>"hunter-male",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Liteknit 3",
+			"cost"=>"620 000",
+			"price"=>"620000",
+			"img"=>"img/Hunter/Liteknit-M-3.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""
+		),
+		array(
+			"name"=>"Hunter Originals",
+			"cost"=>"600 000",
+			"price"=>"600000",
+			"img"=>"img/Hunter/Originals-M-1.jpg",
+			"tag"=>"hunter-male",
+			"status"=>""
+		),
+		array(
+			"name"=>"HunterX 1",
+			"cost"=>"900 000",
+			"price"=>"900000",
+			"img"=>"img/Hunter/HunterX Premium-M-1.jpg",
+			"tag"=>"hunter-male",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"BST 1",
+			"cost"=>"500 000",
+			"price"=>"500000",
+			"img"=>"img/Hunter/BST-F-1.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""
+		),
+		array(
+			"name"=>"BST 2",
+			"cost"=>"600 000",
+			"price"=>"600000",
+			"img"=>"img/Hunter/BST-F-2.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""	
+		),
+		array(
+			"name"=>"Feast 1",
+			"cost"=>"550 000",
+			"price"=>"550000",
+			"img"=>"img/Hunter/Feast-F-1.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""
+		),
+		array(
+			"name"=>"Liteknit 1",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Liteknit-F-1.jpg",
+			"tag"=>"hunter-female",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"Liteknit 2",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Liteknit-F-2.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""
+		),
+		array(
+			"name"=>"Hunter Originals 1",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Originals-F-1.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""
+		),
+		array(
+			"name"=>"Hunter Originals 2",
+			"cost"=>"650 000",
+			"price"=>"650000",
+			"img"=>"img/Hunter/Originals-F-2.jpg",
+			"tag"=>"hunter-female",
+			"status"=>""
+		),
+		array(
+			"name"=>"HunterX 1",
+			"cost"=>"900 000",
+			"price"=>"900000",
+			"img"=>"img/Hunter/HunterX Premium-F-1.jpg",
+			"tag"=>"hunter-female",
+			"status"=>"new"
+		),
+		array(
+			"name"=>"HunterX 2",
+			"cost"=>"900 000",
+			"price"=>"900000",
+			"img"=>"img/Hunter/HunterX Premium-F-2.jpg",
+			"tag"=>"hunter-female",
+			"status"=>"new"
+		)
+	);
+}
+
+?>
